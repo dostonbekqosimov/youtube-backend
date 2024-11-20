18af93b14d23a514e56cb451422fdaff61a7dd10
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 19:58:35 +0500

    Add registration api service method completed

---
+++ src/main/java/dasturlash/uz/repository/EmailHistoryRepository.java
@@ -0,0 +1,32 @@
+package dasturlash.uz.repository;
+
+
+import dasturlash.uz.entity.EmailHistory;
+import dasturlash.uz.enums.EmailStatus;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EmailHistoryRepository extends JpaRepository<EmailHistory, String> {
+    List<EmailHistory> findByProfileIdOrderBySentAtDesc(String profileId);
+    List<EmailHistory> findByToAccountOrderBySentAtDesc(String email);
+
+    List<EmailHistory> findByStatus(EmailStatus status);
+
+//    List<EmailHistory> findByCreatedDate(LocalDateTime date);
+
+    List<EmailHistory> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("update EmailHistory set attemptCount = attemptCount + 1 where id = ?1")
+    void increaseAttemptCount(String id);
+
+    Optional<EmailHistory> findTopByEmailOrderByCreatedDateDesc(String email);
+}
--- src/main/java/dasturlash/uz/service/AuthService.java
+++ src/main/java/dasturlash/uz/service/AuthService.java
@@ -1,10 +1,18 @@
 package dasturlash.uz.service;
 
+import dasturlash.uz.dto.RegistrationDTO;
+import dasturlash.uz.entity.Profile;
+import dasturlash.uz.enums.LanguageEnum;
+import dasturlash.uz.enums.ProfileRole;
+import dasturlash.uz.enums.ProfileStatus;
 import dasturlash.uz.repository.ProfileRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +22,28 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final ResourceBundleService resourceBundleService;
+    private final EmailAuthService emailAuthService;
 
 
+
+
+    public String registerAccount(@Valid RegistrationDTO dto, LanguageEnum lang) {
+
+        Profile profile = new Profile();
+        profile.setName(dto.getName());
+        profile.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+        profile.setSurname(dto.getSurname());
+        profile.setCreatedDate(LocalDateTime.now());
+        profile.setRole(ProfileRole.ROLE_USER);
+        profile.setVisible(Boolean.TRUE);
+        profile.setStatus(ProfileStatus.IN_REGISTERED);
+
+
+        return emailAuthService.registerViaEmail(dto, profile, lang);
+
+    }
+
+    public String resendConfirmationEmail(String id, LanguageEnum lang) {
+        return emailAuthService.resendEmailConfirmation(id, lang);
+    }
 }
