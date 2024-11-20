ffaf1abe29f2d23faa5c8927da39783aa36d0a3d
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 19:59:11 +0500

    Add EmailHistory entity to track email history

---
+++ src/main/java/dasturlash/uz/entity/EmailHistory.java
@@ -0,0 +1,50 @@
+package dasturlash.uz.entity;
+
+
+
+import dasturlash.uz.enums.EmailStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "email_history")
+public class EmailHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String toAccount;
+
+    @Column(nullable = false)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT", length = 65535)
+    private String message;
+
+    @Column(nullable = false)
+    private LocalDateTime sentAt;
+
+    @Enumerated(value = EnumType.STRING)
+    private EmailStatus status;
+
+    private Integer attemptCount = 0;
+
+
+    @Column(columnDefinition = "TEXT")
+    private String errorMessage;
+
+    private LocalDateTime createdDate;
+
+    private String email;
+
+    @Column(name = "profile_id")
+    private String profileId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", updatable = false,insertable = false)
+    private Profile profile;
+}
