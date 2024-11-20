8e75cac90b0f7a669c5250d01f9237e339553af9
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-20 22:42:02 +0500

    added API /changePassword

--- src/main/java/dasturlash/uz/controller/ProfileController.java
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -1,4 +1,24 @@
 package dasturlash.uz.controller;
 
+
+import dasturlash.uz.dto.request.ChangePasswordRequest;
+import dasturlash.uz.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/profile")
 public class ProfileController {
+    @Autowired
+    ProfileService service;
+
+    //API for change password with old password
+    @PostMapping("/changePassword")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
+        return ResponseEntity.ok(service.changePassword(request));
+    }
 }
