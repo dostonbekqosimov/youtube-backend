454374381d27d7aa03a5ee2c77e044a05cd680bd
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-21 00:37:38 +0500

    Add APIs: /create, /getAll

--- src/main/java/dasturlash/uz/controller/ProfileController.java
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -1,14 +1,16 @@
 package dasturlash.uz.controller;
 
 
+import dasturlash.uz.dto.ProfileDTO;
 import dasturlash.uz.dto.request.ChangePasswordRequest;
+import dasturlash.uz.entity.Profile;
 import dasturlash.uz.service.ProfileService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/profile")
@@ -20,5 +22,15 @@ public class ProfileController {
     @PostMapping("/changePassword")
     public ResponseEntity<String> changePassword(@RequestBody ChangePasswordRequest request) {
         return ResponseEntity.ok(service.changePassword(request));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ProfileDTO> create(@RequestBody @Valid ProfileDTO profileDTO) {
+        return ResponseEntity.ok(service.create(profileDTO));
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Profile>> getAll() {
+        return ResponseEntity.ok(service.getAll());
     }
 }
