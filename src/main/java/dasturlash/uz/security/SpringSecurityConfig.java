c7b16b9e7defeac044bccf6c0f280bbb2c610ef3
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 15:10:44 +0500

    Add SpringSecurityConfig class to control security

--- src/main/java/dasturlash/uz/security/SpringSecurityConfig.java
+++ src/main/java/dasturlash/uz/security/SpringSecurityConfig.java
@@ -1,4 +1,20 @@
 package dasturlash.uz.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
 }
