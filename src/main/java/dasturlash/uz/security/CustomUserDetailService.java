396281f729bc7a481b825e6ac01dddd8bdb6e27a
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 15:53:54 +0500

    Finish CustomUserDetailService.java

--- src/main/java/dasturlash/uz/security/CustomUserDetailService.java
+++ src/main/java/dasturlash/uz/security/CustomUserDetailService.java
@@ -1,13 +1,25 @@
 package dasturlash.uz.security;
 
+import dasturlash.uz.entity.Profile;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 public class CustomUserDetailService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Optional<Profile> optional = profileRepository.findByLoginAndVisibleTrue(username);
+
+        if (optional.isEmpty()){
+            throw new UnauthorizedException("Login or password is wrong");
+        }
+
+        Profile profile = optional.get();
+
+
+        return new CustomUserDetails(profile);
     }
 }
