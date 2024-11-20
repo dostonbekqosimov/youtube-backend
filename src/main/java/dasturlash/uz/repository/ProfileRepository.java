3d75c446044b01eb726168c785a185c9eede804b
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 20:01:40 +0500

    Add api for registration

--- src/main/java/dasturlash/uz/repository/ProfileRepository.java
+++ src/main/java/dasturlash/uz/repository/ProfileRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface ProfileRepository extends CrudRepository<Profile, String> {
     Optional<Profile> findByEmailAndVisibleTrue(String username);
+
+    Optional<Profile> findByIdAndVisibleTrue(String id);
 }
