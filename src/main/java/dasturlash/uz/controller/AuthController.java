76669a6519c246e50f8e752e31b21cd140ab2b7c
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 19:58:01 +0500

    Add api for registration

--- src/main/java/dasturlash/uz/controller/AuthController.java
+++ src/main/java/dasturlash/uz/controller/AuthController.java
@@ -1,9 +1,32 @@
 package dasturlash.uz.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dasturlash.uz.dto.RegistrationDTO;
+import dasturlash.uz.enums.LanguageEnum;
+import dasturlash.uz.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/registration")
+    public ResponseEntity<String> registration(@RequestBody @Valid RegistrationDTO dto,
+                                               @RequestHeader(value = "Accept-Language", defaultValue = "uz") LanguageEnum lang) {
+
+        return ResponseEntity.ok(authService.registerAccount(dto, lang));
+    }
+
+    @PostMapping("/registration/resend/{profileId}")
+    public ResponseEntity<String> resendConfirmation(@PathVariable String profileId,
+                                                     @RequestHeader(value = "Accept-Language", defaultValue = "uz") LanguageEnum lang) {
+        return ResponseEntity.ok(authService.resendConfirmationEmail(profileId, lang));
+    }
+
+
 }
