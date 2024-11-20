3ab8186b88bfde6307564496a15d3a6467b80854
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-20 22:42:51 +0500

    Added ChangePasswordRequest

---
+++ src/main/java/dasturlash/uz/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,11 @@
+package dasturlash.uz.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequest {
+    private String oldPassword;
+    private String newPassword;
+}
