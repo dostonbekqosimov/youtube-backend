8b23ac6515c07cd64334a0a02dd55ed09091a5ea
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-20 20:03:06 +0500

    Add EmailHistory entity to track email history

--- src/main/java/dasturlash/uz/service/MessageHistoryService.java
+++ src/main/java/dasturlash/uz/service/MessageHistoryService.java
@@ -1,15 +1,12 @@
 package dasturlash.uz.service;
 
 
-
 import dasturlash.uz.entity.EmailHistory;
 import dasturlash.uz.entity.Profile;
-import dasturlash.uz.entity.SmsHistory;
+
 import dasturlash.uz.enums.EmailStatus;
-import dasturlash.uz.enums.SmsStatus;
 import dasturlash.uz.exceptions.DataNotFoundException;
 import dasturlash.uz.repository.EmailHistoryRepository;
-import dasturlash.uz.repository.SmsHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +16,7 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class MessageHistoryService {
     private final EmailHistoryRepository emailHistoryRepository;
-    private final SmsHistoryRepository smsHistoryRepository;
+
 
     public EmailHistory createEmailHistory(String toEmail, String subject, String message, Profile profile) {
         EmailHistory history = new EmailHistory();
@@ -43,26 +40,5 @@ public class MessageHistoryService {
         emailHistoryRepository.save(history);
     }
 
-    public SmsHistory createSmsHistory(String phone, String verificationCode) {
-        SmsHistory history = new SmsHistory();
-        history.setPhone(phone);
-        history.setVerificationCode(verificationCode);
-        history.setCreatedDate(LocalDateTime.now());
-        history.setAttemptCount(1);
-        history.setStatus(SmsStatus.PENDING_CONFIRMATION);
-        return smsHistoryRepository.save(history);
-    }
 
-    public void updateSmsStatus(SmsHistory history, SmsStatus status) {
-        history.setStatus(status);
-        if (status == SmsStatus.RESENT) {
-            history.setAttemptCount(history.getAttemptCount() + 1);
-        }
-        smsHistoryRepository.save(history);
-    }
-
-    public SmsHistory findLatestSmsHistory(String phoneNumber) {
-        return smsHistoryRepository.findTopByPhoneOrderByCreatedDateDesc(phoneNumber)
-                .orElseThrow(() -> new DataNotFoundException("No SMS history found for phone number: " + phoneNumber));
-    }
 }
