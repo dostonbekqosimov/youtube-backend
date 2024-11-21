ce7ee849eaa46027385995858f4aa760b33436e9
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-21 15:42:44 +0500

    Add CategoryService

---
+++ src/main/java/dasturlash/uz/service/CategoryService.java
@@ -0,0 +1,12 @@
+package dasturlash.uz.service;
+
+import dasturlash.uz.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+}
