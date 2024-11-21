564556ea9515e41bf3b74c47053da838f37320d1
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-21 18:11:42 +0500

    Added body: updateDetail

--- src/main/java/dasturlash/uz/controller/ProfileController.java
+++ src/main/java/dasturlash/uz/controller/ProfileController.java
@@ -43,5 +43,7 @@ public class ProfileController {
     }
 
     @PutMapping("/updateDetail")
-    public ResponseEntity<String> updateDetail(@RequestBody @Valid UpdateProfileDetailDTO dto){}
+    public ResponseEntity<String> updateDetail(@RequestBody @Valid UpdateProfileDetailDTO dto){
+        return ResponseEntity.ok(service.updateDetail(dto));
+    }
 }
