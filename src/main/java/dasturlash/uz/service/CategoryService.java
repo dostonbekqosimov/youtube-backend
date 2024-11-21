30f975b16b51a62328518580e542728a96842ff7
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-21 16:27:40 +0500

    Add update api to update category in CategoryController.java

--- src/main/java/dasturlash/uz/service/CategoryService.java
+++ src/main/java/dasturlash/uz/service/CategoryService.java
@@ -4,6 +4,7 @@ import dasturlash.uz.dto.request.CategoryRequestDTO;
 import dasturlash.uz.dto.response.CategoryResponseDTO;
 import dasturlash.uz.entity.Category;
 import dasturlash.uz.exceptions.DataExistsException;
+import dasturlash.uz.exceptions.DataNotFoundException;
 import dasturlash.uz.repository.CategoryRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +32,28 @@ public class CategoryService {
         return toDto(newCategory);
     }
 
+    public CategoryResponseDTO updateById(Long id, @Valid CategoryRequestDTO requestDTO) {
+
+        // check if it exists
+        // get category
+        Category existingCategory = getById(id);
+
+        // check if the category name exists
+        existsName(requestDTO.getName());
+
+        // update category name
+        existingCategory.setName(requestDTO.getName());
+
+        // set updated date
+        existingCategory.setUpdatedDate(LocalDateTime.now());
+
+        // save new category name in db
+        categoryRepository.save(existingCategory);
+
+        // map to dto and return
+        return toDto(existingCategory);
+    }
+
     public void existsName(String name) {
         boolean isExist = categoryRepository.existsByName(name);
 
@@ -45,5 +68,12 @@ public class CategoryService {
         dto.setName(category.getName());
         dto.setCreatedDate(category.getCreatedDate());
         return dto;
+    }
+
+
+
+    public Category getById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Category with id: " + id + " not found"));
     }
 }
