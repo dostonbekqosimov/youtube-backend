7974b40b236de1af8c87ab9c32fed44f29b69a14
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-21 15:48:02 +0500

    Add CategoryController

---
+++ src/main/java/dasturlash/uz/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package dasturlash.uz.controller;
+
+import dasturlash.uz.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+
+}
