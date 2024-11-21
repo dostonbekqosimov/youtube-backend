4c1baab8ae39926657869002c1849bd8ad97079c
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-21 15:39:26 +0500

    Change id in Category to Long from Sting(UUID)

--- src/main/java/dasturlash/uz/entity/Category.java
+++ src/main/java/dasturlash/uz/entity/Category.java
@@ -11,8 +11,8 @@ import java.time.LocalDateTime;
 @Setter
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
     private String name;
