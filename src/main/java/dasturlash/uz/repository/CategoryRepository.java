8193d08d267cf816928e1096378988e381e37d8b
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-21 15:41:05 +0500

    Add CategoryRepository

---
+++ src/main/java/dasturlash/uz/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package dasturlash.uz.repository;
+
+import dasturlash.uz.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long>, PagingAndSortingRepository<Category, Long> {
+}
