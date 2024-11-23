3f7bb8f04742f9bade81d5a558f5aab7b894b3dd
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-11-24 01:55:04 +0500

    feat: add VideoCreateDTO
    Signed-off-by: dostonbekqosimov <dostonqosimiy19@gmail.com>

--- src/main/java/dasturlash/uz/controller/VideoController.java
+++ src/main/java/dasturlash/uz/controller/VideoController.java
@@ -2,6 +2,7 @@ package dasturlash.uz.controller;
 
 import dasturlash.uz.service.VideoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,6 @@ public class VideoController {
     private final VideoService videoService;
 
     @PostMapping
+    public ResponseEntity<>
 
 }
---
+++ src/main/java/dasturlash/uz/dto/request/video/VideoCreateDTO.java
@@ -0,0 +1,27 @@
+package dasturlash.uz.dto.request.video;
+
+import dasturlash.uz.enums.ContentStatus;
+import dasturlash.uz.enums.VideoType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class VideoCreateDTO {
+    @NotBlank(message = "Your video needs a title")
+    @Size(max = 100, message = "Your title is too long")
+    private String title;
+    @Size(max = 5000, message = "Your description is too long")
+    private String description;
+    @NotNull
+    private Long categoryId;
+    @NotBlank
+    private String attachId;
+    @NotBlank
+    private String previewAttachId;
+    @NotNull
+    private VideoType type;
+
+    private ContentStatus status = ContentStatus.PRIVATE;
+}
