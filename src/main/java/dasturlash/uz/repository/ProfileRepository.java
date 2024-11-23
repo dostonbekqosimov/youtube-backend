466ead750628a2b3b0208f1f5f0ab4680317ed89
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-24 04:29:54 +0500

    Added Query: getProfileDetail

--- src/main/java/dasturlash/uz/repository/ProfileRepository.java
+++ src/main/java/dasturlash/uz/repository/ProfileRepository.java
@@ -1,6 +1,8 @@
 package dasturlash.uz.repository;
 
 import dasturlash.uz.entity.Profile;
+import dasturlash.uz.mapper.ProfileShortInfoMapper;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -11,4 +13,9 @@ public interface ProfileRepository extends CrudRepository<Profile, Long> {
     Optional<Profile> findByIdAndVisibleTrue(Long id);
 
     boolean existsByEmailAndVisibleTrue(String email);
+
+    @Query("select p.id as id, p.name as name, p.surname as surname, p.email as email, p.photoId as photoId " +
+            "from Profile p where p.email = ?1")
+    ProfileShortInfoMapper getProfileShortInfoMapper(String id);
+
 }
