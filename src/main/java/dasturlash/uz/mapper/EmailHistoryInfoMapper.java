093e43bb88091bcb959b6f941e2680c50c4e491a
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-24 01:37:33 +0500

    Added EmailHistoryInfoMapper   -getEmail()   -getTime()

---
+++ src/main/java/dasturlash/uz/mapper/EmailHistoryInfoMapper.java
@@ -0,0 +1,8 @@
+    package dasturlash.uz.mapper;
+
+    import java.time.LocalDateTime;
+
+    public interface EmailHistoryInfoMapper {
+        String getEmail();
+        LocalDateTime getTime();
+    }
