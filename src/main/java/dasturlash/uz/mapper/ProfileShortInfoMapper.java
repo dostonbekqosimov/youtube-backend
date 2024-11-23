078b07e46f39195e303e70ad29c864644d78f718
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-24 04:30:41 +0500

    Added ProfileShortInfoMapper

---
+++ src/main/java/dasturlash/uz/mapper/ProfileShortInfoMapper.java
@@ -0,0 +1,10 @@
+package dasturlash.uz.mapper;
+
+public interface ProfileShortInfoMapper {
+    Long getId();
+    String getName();
+    String getSurname();
+    String getEmail();
+    String getPhotoId();
+
+}
