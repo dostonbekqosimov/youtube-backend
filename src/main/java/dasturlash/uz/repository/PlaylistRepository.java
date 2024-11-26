8d2d5a4e3c9f7b9fb967f89a52c3ef9cb7c125b3
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-11-26 21:28:49 +0500

    Added method: findAllPlaylistOwner

--- src/main/java/dasturlash/uz/repository/PlaylistRepository.java
+++ src/main/java/dasturlash/uz/repository/PlaylistRepository.java
@@ -2,10 +2,13 @@ package dasturlash.uz.repository;
 
 import dasturlash.uz.entity.video.Playlist;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface PlaylistRepository extends CrudRepository<Playlist, String>, JpaRepository<Playlist, String> {
 
@@ -16,4 +19,7 @@ public interface PlaylistRepository extends CrudRepository<Playlist, String>, Jp
     @Transactional
     @Query("update Playlist p set p.visible = false where  p.id = ?1")
     void setVisibleFalse(String playlistId);
+
+    @Query("from Playlist p where p.channelId = ?1")
+    List<Playlist> findAllPlaylistOwner(String channelId);
 }
