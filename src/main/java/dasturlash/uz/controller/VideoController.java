427cfd8cde4bd4e4a034def9d8ac9dbd48e4a936
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-01 09:05:04 +0500

    ......

--- src/main/java/dasturlash/uz/controller/VideoController.java
+++ src/main/java/dasturlash/uz/controller/VideoController.java
@@ -13,7 +13,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import jakarta.servlet.http.HttpServletRequest;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
