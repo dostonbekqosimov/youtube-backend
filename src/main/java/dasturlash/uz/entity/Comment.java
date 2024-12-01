b53849b729a28b125df190b05372b882fb3196f4
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-01 22:43:33 +0500

    feat(comment): add Comment entity
    Signed-off-by: dostonbekqosimov <dostonqosimiy19@gmail.com>

--- src/main/java/dasturlash/uz/entity/Comment.java
+++ src/main/java/dasturlash/uz/entity/Comment.java
@@ -1,4 +1,45 @@
 package dasturlash.uz.entity;
 
+import dasturlash.uz.entity.video.Video;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "comments")
+@Data
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String content;
+
+
+    @Column(name = "profile_id")
+    private Long profileId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", updatable = false, insertable = false)
+    private Profile profile;
+
+    @Column(name = "video_id")
+    private String videoId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "video_id", updatable = false, insertable = false)
+    private Video video;
+
+    @Column(name = "reply_id")
+    private String replyId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_id", updatable = false, insertable = false)
+    private Comment comment;
+
+    @Column(name = "like_count")
+    private Integer likeCount;
+
+    @Column(name = "dislike_count")
+    private Integer dislikeCount;
 }
