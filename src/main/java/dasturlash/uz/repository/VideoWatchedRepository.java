c2cdc0d385177f2f2469dd9238b62fc9c7f06fe7
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-01 17:27:11 +0500

    added query: shu profile korgan videolar

--- src/main/java/dasturlash/uz/repository/VideoWatchedRepository.java
+++ src/main/java/dasturlash/uz/repository/VideoWatchedRepository.java
@@ -4,6 +4,8 @@ import dasturlash.uz.entity.video.VideoWatched;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface VideoWatchedRepository extends CrudRepository<VideoWatched, String> {
 
 
@@ -12,4 +14,7 @@ public interface VideoWatchedRepository extends CrudRepository<VideoWatched, Str
 
     @Query("from VideoWatched v where v.videoId = ?1 and v.ipAddress = ?2 and v.userBrowser = ?3 and v.profileId = ?4 order by v.createdDate desc limit 1")
     VideoWatched findByVideoIdAndIpAndBrowserAndProfileId(String videoId, String ipAddress, String userAgent, Long userId);
+
+    @Query("select vw.videoId as videoId from VideoWatched vw where vw.profileId = ?1")
+    List<String> findByProfileId(Long profileId);
 }
