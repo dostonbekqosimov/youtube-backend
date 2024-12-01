5e5790ecc38826c2e0f97ced074f6f882c5d130e
dostonbekqosimov <dostonqosimiy19@gmail.com>
2024-12-01 22:47:43 +0500

    feat(comment): add controller, service and repository for comment
    Signed-off-by: dostonbekqosimov <dostonqosimiy19@gmail.com>

---
+++ src/main/java/dasturlash/uz/controller/CommentController.java
@@ -0,0 +1,15 @@
+package dasturlash.uz.controller;
+
+import dasturlash.uz.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+}
