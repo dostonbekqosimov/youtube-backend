71ddd4af76d60104512653a524258f1671ea3642
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-09 23:07:44 +0500

    Added query: delete and find by profileId and ChannelId

--- src/main/java/dasturlash/uz/repository/SubscriptionRepository.java
+++ src/main/java/dasturlash/uz/repository/SubscriptionRepository.java
@@ -1,7 +1,17 @@
 package dasturlash.uz.repository;
 
 import dasturlash.uz.entity.Subscription;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
+
+    @Transactional
+    @Modifying
+    @Query("delete from Subscription s where s.profileId = ?1 and s.channelId = ?2")
+    int deleteByProfileIdAndChannelId(Long profileId, String channelId);
+
+    Subscription findByProfileIdAndChannelId(Long profileId, String channelId);
 }
