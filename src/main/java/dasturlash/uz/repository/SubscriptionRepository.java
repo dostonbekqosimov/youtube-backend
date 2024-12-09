f2b9fae8599e29321ac501612159062c8576f332
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-09 22:21:26 +0500

    Added: subscription repository

---
+++ src/main/java/dasturlash/uz/repository/SubscriptionRepository.java
@@ -0,0 +1,7 @@
+package dasturlash.uz.repository;
+
+import dasturlash.uz.entity.Subscription;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
+}
